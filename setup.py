f78883cf3e45e0a965e8f94ad25b24ad41c64273
Tristan Davis <tridav95@gmail.com>
2024-04-28 18:42:22 -0500

    Fixing description format.

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 
 setup(include_package_data = True,
-      long_description=open("README.md").read()
+      long_description=open("README.md").read(),
+      long_description_content_type='text/markdown'
       )
