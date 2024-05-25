6b963010c180bf2a26bb8ee8422bdd93dbf8bd12
Tristan Davis <tridav95@gmail.com>
2024-05-25 16:47:46 -0500

    Updates to descriptions.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+project_urls = {
+    'github':'https://github.com/trisdav/htmlxword/tree/main'
+}
 
 setup(include_package_data = True,
+      description="Create Static HTML/Js Crosswords",
       long_description=open("README.md").read(),
-      long_description_content_type='text/markdown'
+      long_description_content_type='text/markdown',
+      project_urls = project_urls,
       )
